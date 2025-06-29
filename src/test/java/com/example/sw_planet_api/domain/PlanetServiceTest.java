81786c8a7b4e83929a5f6d04ec4e6e8bb546efcb
Lucas Fonseca <lucasilva.fonseca@gmail.com>
2025-06-29 19:20:07 -0300

    Teste de ID existente e inexistente

--- src/test/java/com/example/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/example/sw_planet_api/domain/PlanetServiceTest.java
@@ -8,10 +8,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Optional;
+
 import static com.example.sw_planet_api.common.PlanetConstants.INVALID_PLANET;
 import static com.example.sw_planet_api.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 //@SpringBootTest(classes = PlanetService.class)
@@ -31,17 +34,31 @@ public class PlanetServiceTest {
 
         //system under test
         Planet sut = planetService.create(PLANET);
-
         assertThat(sut).isEqualTo(PLANET);
-        System.out.println(PLANET);
     }
 
     @Test
     public void createPlanetWithInvalidDataThrowsException(){
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
-
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    public void getPlanetByExistingIdReturnsPlanet(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
 
+        Optional <Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanetByUnexistingIdReturnsEmpty(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isEmpty();
+    }
 }
